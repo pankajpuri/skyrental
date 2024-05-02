d67c6438000fa2baeb9efe029658bd98bcf2ae78
Pankaj Puri <pankajpuri016@gmail.com>
2024-05-02 22:03:58 +1200

    initial commit (#1)

---
+++ skyrental/Models/Properties.cs
@@ -0,0 +1,11 @@
+﻿namespace skyrental.Models
+{
+    public class Properties
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public double Rent { get; set; }
+        public string PropertyType { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ skyrental/Pages/Properties/AddProperties.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace skyrental.Pages.Properties
+{
+    public class AddPropertiesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
